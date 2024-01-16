9cddda86785c338756f0c707d2a7926bae2b0831
Miikakyl <kylmanen.miika@gmail.com>
2024-01-16 21:02:25 +0200

    PassNoteParameters added to pass values between components, signout function and createEmptyNote function added

--- src/App.js
+++ src/App.js
@@ -1,38 +1,86 @@
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import { FIREBASE_AUTH } from "./firebaseConfig.js";
 import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+import AlertMsg from "./components/AlertMsg.js";
+
 import Sidebar from "./components/Sidebar";
 import Notes from "./components/Notes";
 import Writing from "./components/Writing";
 import AuthenticationModal from "./components/AuthenticationModal";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./styles/styles.css"
 
 const App = () => {
     const [isSignIn, setIsSignIn] = useState(true)
+    const [userData, setUserData] = useState("")
+    const [note, setNote] = useState("")
+    const [noteHighlightSwitch, setNoteHighlightSwitch] = useState(false)
 
     useEffect(() => {
         onAuthStateChanged(FIREBASE_AUTH, (user) => {
             if (user) {
-              const uid = user.uid;
-              console.log(uid)
-              setIsSignIn(true)
+                setUserData({
+                    uid: user.uid,
+                    email: user.email
+                })
+                setIsSignIn(true)
+                const emptyNote = createEmptyNote()
+                passNoteParameters(emptyNote)
             } else {
                 setIsSignIn(false)
             }
         })
     }, [])
 
-    
-    return (
-        <div className="container-fluid h-100" style={{overflow: "hidden"}} >
-            <div className="row d-flex h-100">
-               {isSignIn? null : <AuthenticationModal />}
-                <Sidebar />
-                <Notes />
-                <Writing />
+    const handleSignOut = () => {
+        signOut(FIREBASE_AUTH).then(() => {
+            setIsSignIn(false)
+        }).catch((error) => {
+            console.log(error)
+        });
+    }
+
+    const createEmptyNote = () => {
+        const newNoteId = uuidv4()
+        const note = {
+            header: "",
+            text: "",
+            timestamp: "1D",
+            id: newNoteId
+        }
+        return note
+    }
+
+    const passNoteParameters = (note) => {
+        if (!note) {
+            note = createEmptyNote()
+            setNoteHighlightSwitch(!noteHighlightSwitch)
+        }
+        setNote(note)
+    }
+
+
+        return (
+            <div className="container-fluid h-100 position-relative" style={{ overflowX: "hidden" }}>
+                {/* <AlertMsg /> */}
+                <div className="row d-flex h-100">
+                    {isSignIn ? null : <AuthenticationModal />}
+                    <Sidebar createNewNote={passNoteParameters} />
+                    <Notes
+                        uid={userData.uid}
+                        passNoteParameters={passNoteParameters}
+                        email={userData.email}
+                        signOut={handleSignOut}
+                        noteHighlightSwitch={noteHighlightSwitch}
+                    />
+                    <Writing 
+                        uid={userData.uid} note={note}
+                        isSignIn={isSignIn}
+                    />
+                </div>
             </div>
-        </div>
-    );
-}
- 
+        )
+    }
+
 export default App;
